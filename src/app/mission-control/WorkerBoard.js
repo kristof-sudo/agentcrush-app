25a72ac0afc0af8cd0f186afd2f4b887834abe5f
kristof-sudo <264957887+kristof-sudo@users.noreply.github.com>
2026-03-13 15:07:28 +0100

    Update WorkerBoard.js

--- src/app/mission-control/WorkerBoard.js
+++ src/app/mission-control/WorkerBoard.js
@@ -1,1 +1,29 @@
+'use client'
 
+import { useEffect, useState } from 'react'
+
+export default function WorkerBoard() {
+  const [runs, setRuns] = useState([])
+
+  useEffect(() => {
+    fetch('/api/mission-control/workers')
+      .then(res => res.json())
+      .then(data => setRuns(data.runs || []))
+  }, [])
+
+  return (
+    <div className="space-y-2">
+      {runs.map((r) => (
+        <div
+          key={r.id}
+          className="p-3 bg-white/5 rounded-lg border border-white/10"
+        >
+          <div className="font-medium">{r.runner}</div>
+          <div className="text-sm opacity-70">
+            Status: {r.status}
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
